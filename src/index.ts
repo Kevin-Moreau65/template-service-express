7b7ba9c398dfa42258dd8973dfd279409c76ccbf
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-12-12 18:55:11 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.status(200).send('Test ci 1'));
+app.get('/', (req, res) => res.status(200).send('Test ci 2 Tout est OK'));
 app.use('*', (req, res) => res.status(404).send('Retour arrière frérot'));
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Le serveur est OK sur le port ${process.env.SERVER_PORT}`);
