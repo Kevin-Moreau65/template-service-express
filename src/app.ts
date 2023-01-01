26907f3b94faef716ca9250dd62345dc221bfc8c
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-01-01 12:14:21 +0100

    fixed alias

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ const createServer = () => {
   app.use(cors());
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true }));
-  app.get('/', (req, res) => res.status(200).send('Test ci 2 Tout est OK'));
-  app.use('*', (req, res) => res.status(404).send('Retour arrière frérot'));
+  app.get('/', (req, res) => res.status(200).send('OK'));
+  app.use('*', (req, res) => res.status(404).send('NOT FOUND'));
   return app;
 };
 
