36817742af794b180e50f8e276ab7d0f29027295
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-01-01 17:23:10 +0100

    fixed test

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 
 const createServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true }));
-  app.get('/', (req, res) => res.status(200).send('OK'));
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: true,
+    }),
+  );
+  app.use(
+    helmet.dnsPrefetchControl({
+      allow: true,
+    }),
+  );
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+  app.use(helmet({ crossOriginEmbedderPolicy: false }));
+  app.get('/', (req, res) => res.send('OK'));
   app.use('*', (req, res) => res.status(404).send('NOT FOUND'));
   return app;
 };
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,6 @@
-import 'module-alias/register';
 import createServer from '@/app';
-import helmet from 'helmet';
 const startServer = () => {
   const app = createServer();
-  app.use(helmet());
-  app.use(
-    helmet.contentSecurityPolicy({
-      useDefaults: true,
-    }),
-  );
-  app.use(
-    helmet.dnsPrefetchControl({
-      allow: true,
-    }),
-  );
-  app.use(helmet.hidePoweredBy());
-  app.use(helmet.noSniff());
-  app.use(helmet.xssFilter());
-  app.use(helmet({ crossOriginEmbedderPolicy: false }));
   app.listen(process.env.SERVER_PORT || 8080, () => {
     console.log(
       `Le serveur est OK sur le port ${process.env.SERVER_PORT || 8080}`,
